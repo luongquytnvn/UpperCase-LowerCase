ed00ca2d7eaa47b0dfb8f749e49ed47dcba01b57
luongquytnvn <luongquytnvn@gmail.com>
2019-10-15 15:43:11 +0700

    upper

---
+++ script.js
@@ -0,0 +1,13 @@
+let regexp = /^[A-Z]/;
+isFirstLetterUpperCase('Abcd');
+   isFirstLetterUpperCase('abcd')
+function isFirstLetterUpperCase(str) {
+    regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+        console.log("String's first character is uppercase");
+    } else {
+        console.log("String's first character is not uppercase");
+    }
+}
+
+
